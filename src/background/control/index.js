286c9bb767b250a5e9fb3d040367b43f11e5c974
水华 <mizuka.wu@outlook.com>
2021-01-13 23:31:31 +0800

    feat: flv

---
+++ src/background/control/index.js
@@ -0,0 +1,6 @@
+/**
+ * 控制控制
+ */
+import vm from 'vm'
+console.log(vm)
+ export const playList = []
---
+++ src/background/media/index.js
@@ -0,0 +1,21 @@
+/**
+ * 视频服务，播放服务器
+ */
+import NodeMediaServer from 'node-media-server'
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
