47bb06fb67dd59591e97fa1e69e860f8e46b9ec7
水华 <mizuka.wu@outlook.com>
2021-01-13 22:35:12 +0800

    feat: source配置

---
+++ src/configs/resource.js
@@ -0,0 +1,10 @@
+/**
+ * 资源定义
+ */
+import SOURCE from './source'
+
+export const VIDEO_WALLPAPER = {
+    [SOURCE.OFFICLE]: 'https://dynamic-wallpaper.github.io/video-wallpaper/',
+    [SOURCE.GITHUB]: 'https://raw.githubusercontent.com/dynamic-wallpaper/video-wallpaper/gh-pages/index.html',
+    [SOURCE.JSDELIVR]: 'https://cdn.jsdelivr.net/gh/dynamic-wallpaper/video-wallpaper@gh-pages/index.html'
+}
---
+++ src/configs/source.js
@@ -0,0 +1,22 @@
+/**
+ * 定义CDN
+ */
+
+export const SOURCE = {
+    /**
+     * 官方
+     */
+    OFFICLE: 'dynamic-wallpaper',
+    /**
+     * GITHUB
+     */
+    GITHUB: 'gh',
+    /**
+     * JSDELIVR
+     */
+    JSDELIVR: 'jsdelivr'
+}
+
+export const DEFAULT_SOURCE = SOURCE.JSDELIVR
+
+export default SOURCE
