50c6ec68cab2a9ca75b1d4aee83ad4f4aa1cab02
水华 <yiwei.wu@mihoyo.com>
2021-03-23 11:37:53 +0800

    feat: 开机自启动

--- src/background/control/tray.js
+++ src/background/control/tray.js
@@ -1,15 +1,18 @@
 const { Menu, Tray } = require('electron')
-
+const ex = process.execPath
 let tray = null
+const context = {
+  store: null,
+  openControlBrowser: null,
+  app: null
+}
 
 /**
- *
- * @param {Electron.App} app
- * @param {import('electron-store')} store
- * @param {*} openControlBrowser
+ * 构建菜单
  */
-export default function (app, store, openControlBrowser) {
-  tray = new Tray(`${__static}/icons/png/16x16.png`)
+function buildContextMenu () {
+  const { app, openControlBrowser, store } = context
+  const openAtLogin = store.get('openAtLogin')
   const contextMenu = Menu.buildFromTemplate([
     {
       label: '选择桌面',
@@ -18,12 +21,45 @@ export default function (app, store, openControlBrowser) {
       }
     },
     {
+      label: '开机自启动',
+      type: 'checkbox',
+      checked: openAtLogin,
+      click () {
+        app.setLoginItemSettings({
+          openAtLogin: !openAtLogin,
+          path: ex,
+          args: []
+        })
+        store.set('openAtLogin', !openAtLogin)
+        buildContextMenu()
+      }
+    },
+    {
+      type: 'separator'
+    },
+    {
       label: '退出',
       click () {
         app.exit()
       }
     }
   ])
-  tray.setToolTip('动态壁纸')
   tray.setContextMenu(contextMenu)
+}
+
+/**
+ * 初始化Tray
+ * @param {Electron.App} app
+ * @param {import('electron-store')} store
+ * @param {*} openControlBrowser
+ */
+export default function (app, store, openControlBrowser) {
+  tray = new Tray(`${__static}/icons/png/16x16.png`)
+
+  context.app = app
+  context.store = store
+  context.openControlBrowser = openControlBrowser
+
+  tray.setToolTip('动态壁纸')
+  buildContextMenu()
 }
