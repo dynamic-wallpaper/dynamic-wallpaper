f539b58e5743612212a6052bec4e2c53ba099bbd
水华 <mizuka.wu@outlook.com>
2021-03-25 22:55:20 +0800

    feat: 增加自动生成mutation

--- src/store/vuex.js
+++ src/store/vuex.js
@@ -3,24 +3,45 @@
  */
 import { getState, schema } from './schema'
 import Vuex from 'vuex'
+import throttle from 'lodash/throttle'
 
 export default function (ipcRenderer) {
+  const syncToBackground = throttle(function (state) {
+    ipcRenderer.send('vuex:sync', state)
+  }, 500)
+
   return new Vuex.Store({
     state: getState(schema),
     mutations: {
-      increment (state) {
-        state.count++
-      }
+      /**
+       * 和后台同步
+       */
+      'vuex:sync': function (state, electronStore) {
+        Object.assign(state, electronStore)
+      },
+      // 自动遍历生成mutations
+      ...Object.fromEntries(Object.keys(schema).map(key => [`set:${key}`, function (state, payload) {
+        state[key] = payload
+      }]))
     },
     plugins: [(store) => {
+      /**
+       * vuex:init事件相关，告诉后台已经初始化，需要同步
+       */
       ipcRenderer.send('vuex:init')
-      store.subscribe((mutation, state) => {
-        // 每次 mutation 之后调用
-        // mutation 的格式为 { type, payload }
-        console.log(mutation, state, ipcRenderer)
+      ipcRenderer.on('vuex:sync', (e, state) => {
+        store.commit('vuex:sync', state)
       })
-      ipcRenderer.on('vuex:mutation', (e, a) => {
-        console.log(a)
+
+      /**
+       * 像后台推送变化
+       */
+      store.subscribe((mutation, state) => {
+        if (mutation.includes('vuex:sync')) {
+          return
+        }
+
+        syncToBackground(state)
       })
     }]
   })
