4604f14fdc98c71840cf5df9aa2147cd213484bc
水华 <mizuka.wu@outlook.com>
2021-04-04 14:30:13 +0800

    Squashed commit of the following:
    commit ff713c835d52065a846b0c666c0af711e7bc6280
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Sat Apr 3 09:08:42 2021 +0800
    
        1
    
    commit 43c0bba6a134d8f941c3f7478b6f04e77162dca3
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Fri Apr 2 22:18:59 2021 +0800
    
        remove win32
    
    commit 36f0f337464af04b93455aefa0c5a422f4d3dbd3
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Fri Apr 2 22:17:32 2021 +0800
    
        feat: 注释gpu加速
    
    commit f83796f47e8d3e11b64554d8cc6be182c2d30599
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Fri Apr 2 22:12:31 2021 +0800
    
        硬解
    
    commit 468d244b2408e6d77f185bb3dab7d3b8f9efa2bc
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Fri Apr 2 22:05:53 2021 +0800
    
        feat: 绘制方案修改
    
    commit 31139ce0d47647a6302f79fe90bfff5286c41426
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Fri Apr 2 21:52:33 2021 +0800
    
        feat: 修复宽度高度
    
    commit 3e073cfc091cb95aff93ca9d6c5a636aba93fe60
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Fri Apr 2 21:40:04 2021 +0800
    
        feat: 事件调整
    
    commit 697a5078a71cd0534d3dece9c52d5bf78c19c65b
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Fri Apr 2 21:32:29 2021 +0800
    
        feat: 设置url
    
    commit efab9bcb21cf7af7e043a2ee8b2d7e81f7524d7b
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Fri Apr 2 21:18:31 2021 +0800
    
        feat: 播放
    
    commit 44afa6e91ceec5722e05aab1c47d8c46fcf1a00b
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Fri Apr 2 21:17:26 2021 +0800
    
        feat: 播放控制
    
    commit 55dfba070c425bde146a31a05d3989ba3e8f8ffe
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Mon Mar 29 21:56:07 2021 +0800
    
        feat: 修改算法
    
    commit db89b7b634da11ecf275003118acddf7b1ab92b2
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Sun Mar 28 23:02:32 2021 +0800
    
        feat: 超级调整
    
    commit 1ab91ebd435aa294b366db68bbd44778e6e782ff
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Sun Mar 28 22:45:41 2021 +0800
    
        feat: ffmpeg策略
    
    commit a903109009eb70d031f74ce39bee686b4dee5452
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Sun Mar 28 20:41:11 2021 +0800
    
        feat: 移除fluent ffmpeg
    
    commit 1c0c1caf0d4b20c079baeeffebb019fc8346f2e8
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Sun Mar 28 10:27:01 2021 +0800
    
        fix: decodeUrl
    
    commit d0c8f513616875ba19c9bf7566cde2250edd2559
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Sun Mar 28 10:22:34 2021 +0800
    
        feat: 增加mediaService，decodeUrl修改
    
    commit 6992e5dc7f0b6a72c494fed43ececb2979bf0615
    Author: 水华 <mizuka.wu@outlook.com>
    Date:   Sat Mar 27 22:15:56 2021 +0800
    
        feat: 打包ffmpeg

---
+++ src/pages/player/renderer/canvas.vue
@@ -0,0 +1,89 @@
+<template>
+  <canvas style="width: 100%; height: 100%;" ref="canvas" />
+</template>
+
+<script>
+import renderer from './renderer'
+import throttle from 'lodash/throttle'
+const ipcRenderer = window.ipcRenderer
+
+export default {
+  extends: renderer,
+  name: 'canvasRenderer',
+  data () {
+    const vm = this
+    return {
+      video: {
+        width: 0,
+        height: 0
+      },
+      screen: {
+        offsetWidth: 0,
+        offsetHeight: 0
+      },
+      frameHandler: function (e, data) {
+        vm.draw(data)
+      }
+    }
+  },
+  computed: {
+    /**
+     * 参数根据具体值来算，通过缓存数据保证最大限度节省资源
+     */
+    drawImageArgs ({ screen, video }) {
+      const videoWidth = video.width
+      const videoHeight = video.height
+      const { offsetWidth, offsetHeight } = screen
+
+      let offsetX = 0
+      let offsetY = 0
+
+      if (videoWidth / videoHeight > offsetWidth / offsetHeight) {
+        const maxVideoWidth = offsetWidth * videoHeight / offsetHeight
+        offsetX = (videoWidth - maxVideoWidth) / 2
+      } else {
+        const maxVideoHeight = offsetHeight * videoWidth / offsetWidth
+        offsetY = (videoHeight - maxVideoHeight) / 2
+      }
+      return [offsetX, offsetY, videoWidth - offsetX, videoHeight - offsetY, 0, 0, offsetWidth, offsetHeight]
+    }
+  },
+  methods: {
+    getScreenSize: throttle(function () {
+      const { offsetWidth, offsetHeight } = document.body
+      this.screen.offsetWidth = offsetWidth
+      this.screen.offsetHeight = offsetHeight
+    }, 100),
+    draw (base64) {
+      const src = 'data:image/jpeg;base64,' + base64
+      const img = new Image()
+      img.src = src
+      img.onload = () => {
+        const { drawImageArgs, screen } = this
+        const canvas = this.$refs.canvas
+        const context = canvas.getContext('2d')
+        this.video.width = img.width
+        this.video.height = img.height
+        const { offsetWidth, offsetHeight } = screen
+
+        canvas.width = offsetWidth
+        canvas.height = offsetHeight
+
+        context.drawImage(img, ...drawImageArgs)
+      }
+    }
+  },
+  mounted () {
+    this.getScreenSize()
+    window.addEventListener('resize', this.getScreenSize)
+    ipcRenderer.on('player:frame', this.frameHandler)
+  },
+  destroyed () {
+    window.removeEventListener('resize', this.getScreenSize)
+    ipcRenderer.off('player:frame', this.frameHandler)
+  }
+}
+</script>
+
+<style>
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-template-curly-in-string */
 module.exports = {
   pages: {
     index: 'src/pages/main/index.js',
@@ -9,21 +10,9 @@ module.exports = {
       preload: 'src/pages/preload.js',
       mainProcessFile: 'src/background/index.js',
       mainProcessWatch: ['src/background/**/*'],
-      chainWebpackMainProcess: (config) => {
-        config.plugin('define').tap(definitions => {
-          definitions[0] = {
-            ...definitions[0],
-            'process.env.FLUENTFFMPEG_COV': false
-          }
-          return definitions
-        })
-      },
       builderOptions: {
-        // eslint-disable-next-line no-template-curly-in-string
-        artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
-        // extraFiles: [
-        //   './node_modules/ffmpeg-static/ffmpeg*'
-        // ],
+        artifactName: '${productName}-${version}-${platform}-${arch}.${ext}',
+        extraResources: ['./ffmpeg/${platform}-${arch}'],
         mac: {
           icon: './public/icons/mac/icon.icns',
           target: {
