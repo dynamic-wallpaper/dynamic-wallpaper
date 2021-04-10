270ae69bd46f5fc8fd1197f3899efa8ec51d99bb
水华 <mizuka.wu@outlook.com>
2021-04-10 23:07:18 +0800

    fix: target

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
           icon: './public/icons/mac/icon.icns',
           target: {
             arch: ['arm64', 'x64'],
-            target: 'dmg'
+            target: 'default'
           }
         }
       }
