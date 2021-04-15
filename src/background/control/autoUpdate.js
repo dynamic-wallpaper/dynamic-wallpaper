0304fe923ad016b16ce0420c31716554f58886d7
水华 <mizuka.wu@outlook.com>
2021-04-15 22:19:44 +0800

    feat: 更新方式更新

--- src/background/control/autoUpdate.js
+++ src/background/control/autoUpdate.js
@@ -1,5 +1,54 @@
 import { autoUpdater } from 'electron-updater'
+import { dialog } from 'electron'
+import logger from 'electron-log'
+
+autoUpdater.logger = logger
+autoUpdater.autoDownload = false
 
 export default function () {
-  autoUpdater.checkForUpdatesAndNotify()
+  autoUpdater.checkForUpdates()
+
+  // listen for update found
+  autoUpdater.on('update-available', () => {
+    // prompt user to start download
+    dialog.showMessageBox({
+      type: 'info',
+      title: '升级版本',
+      message: '发现了新版本，你是否现在可以升级？',
+      buttons: [
+        '现在升级',
+        '以后再说'
+      ]
+    })
+      .then(answer => {
+        const buttonIndex = answer.response
+        if (buttonIndex === 0) autoUpdater.downloadUpdate()
+      })
+      .catch(error => {
+        console.error(error, 'error in update-available')
+      })
+  })
+
+  // listen for download being ready
+  autoUpdater.on('update-downloaded', () => {
+    // prompt user to install update
+    dialog.showMessageBox({
+      type: 'info',
+      title: '已准备好升级',
+      message: '是否立马重启？',
+      buttons: [
+        '重启',
+        '取消'
+      ]
+    })
+      .then(answer => {
+        const buttonIndex = answer.response
+
+        // install and restart if answer is 0 "Sure!"
+        if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true)
+      })
+      .catch(error => {
+        console.error(error, 'error in update-downloaded')
+      })
+  })
 }
