6c8fae30641be3e0b3e56cecf722bb259f0d5953
Edward Mizuka <mizuka.wu@outlook.com>
2021-04-16 11:43:47 +0800

    增加背景

--- src/configs/website.js
+++ src/configs/website.js
@@ -3,9 +3,24 @@ export default {
   key: 'website',
   value: [
     {
+      label: '翻页时钟',
+      description: 'https://www.17sucai.com/preview/11/2017-08-14/3/demo.html',
+      value: 'https://www.17sucai.com/preview/11/2017-08-14/3/demo.html'
+    },
+    {
+      label: '转盘时钟',
+      description: 'https://www.17sucai.com/preview/1671574/2021-01-18/test/index.html',
+      value: 'https://www.17sucai.com/preview/1671574/2021-01-18/test/index.html'
+    },
+    {
+      label: '数字滑动时钟',
+      description: 'https://www.17sucai.com/preview/11/2017-07-19/sz/demo.html',
+      value: 'https://www.17sucai.com/preview/11/2017-07-19/sz/demo.html'
+    },
+    {
       label: '数字雨',
-      description: 'https://tidwall.com/digitalrain/',
-      value: 'https://tidwall.com/digitalrain/'
+      description: 'https://www.17sucai.com/preview/1424582/2020-03-29/jz/index.html',
+      value: 'https://www.17sucai.com/preview/1424582/2020-03-29/jz/index.html'
     },
     {
       label: '元素周期表',
